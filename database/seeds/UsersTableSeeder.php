8527ef8feaba86489fcbfec33d6f1e6e860ae185
Argos <fires3as0n@gmail.com>
2019-01-15 14:12:20 +0000

    hotfix

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,7 @@ class UsersTableSeeder extends Seeder
 	 */
 	public function run()
 	{
-		$path = base_path() . "\\database\\seeds\\UsersSeederData.txt";
+		$path = base_path() . "/database/seeds/UsersSeederData.txt";
 		$file = File::get($path);
 		$data = json_decode($file, true);
 		DB::table('users')->insert($data);
