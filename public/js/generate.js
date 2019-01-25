3f536664f78f25fd904c7df437f141236464abe3
Argos <fires3as0n@gmail.com>
2019-01-25 19:34:50 +0000

    1

--- public/js/generate.js
+++ public/js/generate.js
@@ -1,1 +1,229 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=0)}({0:function(e,t,n){e.exports=n(5)},5:function(e,t,n){"use strict";function r(e,t){var n=0-(new Date).getTimezoneOffset()/60,r=[31,28,31,30,31,30,31,31,30,31,30,31];29===new Date(e,2,0,n).getDate()&&(1,r[1]+=1);for(var o=["","ПН","ВТ","СР","ЧТ","ПТ","СБ","ВС"],u=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],a=Array(),i=0,c=0;i<12;i++)for(var f=0;f<r[i];f++,c++){var l={},d=new Date(e,i,f+1,n);l.id=t+c+1,l.day_in_week_number=0==d.getDay()?7:d.getDay(),l.day_in_month_number=f+1,l.day_name=o[l.day_in_week_number],l.month_number=i+1,l.month_name=u[i],l.year=e,a[c]=l}return[a,a.length]}n.r(t);var o=document.querySelector("#form"),u=document.querySelector("#data-field");document.querySelector("#year");document.querySelector("#button").addEventListener("click",function(){new Promise(function(e,t){for(var n=Array(),o=0,u=2019;u<2026;u++){var a=r(u,o);o+=a[1],n=n.concat(a[0])}e(n)}).then(function(e){console.table(e),e=JSON.stringify(e),u.value=e,o.submit()})})}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/DaysJsonGenerator.js":
+/*!*******************************************!*\
+  !*** ./resources/js/DaysJsonGenerator.js ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return generateDaysJson; });
+function generateDaysJson(year, id) {
+  /*get local date*/
+  var now = new Date();
+  /*Use offset to create a number used for making	real time
+  This helps to overcome bug in JS Date object
+  
+  In oder to get real time, always pass offset
+  instead of hours and then add the desired time
+  
+  offset variable represents 0:00 time for current time zone
+  
+  -Usage
+  To get: 1970.01.01-04:00
+  Pass: Date(1970, 0, 1, offset + 4)
+  -Yes, in this shit language months counts from 0
+   but days from 1
+  -If you pass only 4, you will get not the desired time but
+   the time that UTC had at that moment
+  */
+
+  var offset = 0 - now.getTimezoneOffset() / 60;
+  /*Get days in February
+  * passing month next to desired one and 0 as day, returns
+  * the last day of desired month - a neat lifehack
+  * */
+
+  var feb = new Date(year, 2, 0, offset).getDate();
+  /*seeding data*/
+
+  var total_days = 365;
+  var days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  if (feb === 29) {
+    total_days += 1;
+    days_in_month[1] += 1;
+  }
+
+  var week_day_names = ["", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
+  var month_names = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+  var result = Array();
+
+  for (var month = 0, iterator = 0; month < 12; month++) {
+    for (var day = 0; day < days_in_month[month]; day++, iterator++) {
+      var obj = {};
+      var date = new Date(year, month, day + 1, offset);
+      obj["id"] = id + iterator + 1;
+      obj["day_in_week_number"] = date.getDay() == 0 ? 7 : date.getDay();
+      obj["day_in_month_number"] = day + 1;
+      obj["day_name"] = week_day_names[obj["day_in_week_number"]];
+      obj["month_number"] = month + 1;
+      obj["month_name"] = month_names[month];
+      obj["year"] = year;
+      result[iterator] = obj;
+    }
+  } //console.table(result);
+
+
+  return [result, result.length];
+}
+
+/***/ }),
+
+/***/ "./resources/js/generate.js":
+/*!**********************************!*\
+  !*** ./resources/js/generate.js ***!
+  \**********************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _DaysJsonGenerator_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./DaysJsonGenerator.js */ "./resources/js/DaysJsonGenerator.js");
+
+var form = document.querySelector('#form');
+var data_field = document.querySelector('#data-field');
+var input = document.querySelector('#year');
+var button = document.querySelector('#button');
+button.addEventListener('click', submitHandler);
+
+function submitHandler() {
+  waitForResult().then(function (result) {
+    console.table(result);
+    result = JSON.stringify(result); //console.log(result);
+
+    data_field.value = result;
+    form.submit();
+  });
+}
+
+function waitForResult() {
+  return new Promise(function (resolve, reject) {
+    var result = Array();
+    var id = 0;
+
+    for (var i = 2019; i < 2026; i++) {
+      var response = Object(_DaysJsonGenerator_js__WEBPACK_IMPORTED_MODULE_0__["default"])(i, id);
+      id += response[1];
+      result = result.concat(response[0]);
+    }
+
+    resolve(result);
+  });
+} //<editor-fold desc="Promise testing function">
+//function waitForResult(value)
+//{
+//	return new Promise( (resolve, reject) => {
+//		setTimeout( () => {
+//			if (value > 1000)
+//				resolve("resolved");
+//			else
+//				reject("rejected");
+//		}, 2000);
+//	});
+//}
+//</editor-fold>
+
+/***/ }),
+
+/***/ 0:
+/*!****************************************!*\
+  !*** multi ./resources/js/generate.js ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! E:\Code\Graph\resources\js\generate.js */"./resources/js/generate.js");
+
+
+/***/ })
+
+/******/ });
